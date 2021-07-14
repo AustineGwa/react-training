0e73ddc9056daa3dcd6359dd5611291f7ea10c71
AustineGwa <austinegwa64@gmail.com>
2021-07-14 13:08:47 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
+
+
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
+
+const name = "Austine Gwa"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Header />
+      <h1>Hello from react </h1>
+      <h3>Mr, {name}</h3>
+     
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class App extends React.Component{
+    render(){
+        return <h6>Demo App</h6>
+    }
+}
+
+export default App
